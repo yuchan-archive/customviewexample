91e3d0be8769ca5b255462415f52379c657d2675
Yusuke Ohashi <github@junkpiano.me>
2015-08-04 19:51:59 +0900

    Initial Commit

---
+++ CustomViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomViewExample
+//
+//  Created by Ohashi, Yusuke a on 8/4/15.
+//  Copyright (c) 2015 Yusuke Ohashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
